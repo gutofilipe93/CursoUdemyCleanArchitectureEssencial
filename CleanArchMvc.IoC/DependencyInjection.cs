25660dda113b74b2e9b7d0d9d79ca7214bd5096f
Gustavo Filipe <gutofilipe93@gmail.com>
2021-12-27 17:34:35 -0300

    Problemas com o migrations

---
+++ CleanArchMvc.IoC/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CleanArchMvc.Data.Context;
+using CleanArchMvc.Data.Repositories;
+using CleanArchMvc.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMvc.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;    
+        }
+    }
+}
